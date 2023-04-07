6bf23819c8a348c4b8fc0a8f5f0f4158a057b5df
Douglas Braga <30268535+dobraga@users.noreply.github.com>
2023-04-07 00:17:20 -0300

    add test

---
+++ test/game_test.ts
@@ -0,0 +1,58 @@
+import { assertNotEquals, assertThrows } from "assert";
+
+import createGame from "../app/static/utils/game.js";
+import createDeck from "../app/static/utils/deck.ts";
+
+const game = createGame();
+const deck = createDeck();
+
+const user1 = {
+  id: 235432623,
+  username: "teste12",
+  cards: deck.white(10),
+  round: {
+    "card": deck.black(),
+    "player": 235432623,
+  },
+};
+
+const user2 = {
+  id: 785685685,
+  username: "teste45",
+  cards: deck.white(10),
+};
+
+Deno.test("Setup game", () => {
+  game.setOwnerRound(user1);
+  game.addPlayer(user1);
+  game.setCardsHand(user1);
+
+  game.addPlayer(user2);
+  game.setCardsHand(user2);
+});
+
+Deno.test("Select card and set winner", () => {
+  const selected_card = user2.cards.slice(0, user1.round.card.qtdSpaces)
+  const command = {
+    id: user2.id,
+    cards: selected_card,
+    nextCard: deck.white(game.state.round.qtdSpaces),
+  };
+  game.finishRound(command);
+
+  const command_win = {
+    winner: user2.id,
+    cards: selected_card,
+    answer: user1.round.card,
+    newRound: deck.black()
+  }
+  const old_white_cards = JSON.stringify(game.hand[user2.id]);
+  game.setWinnerSetupNextTurn(command_win)
+
+  assertNotEquals(old_white_cards, JSON.stringify(game.hand[user2.id]))
+
+
+  console.log(game.state);
+  console.log(game.hand);
+  console.log(game.wins);
+});
