653336265121690f6c92a1f2812d5de2ce03e8db
Douglas Braga <30268535+dobraga@users.noreply.github.com>
2023-04-27 08:08:49 -0300

    reestruct

--- app/app.ts
+++ app/app.ts
@@ -2,8 +2,8 @@ import { Server } from "socketio";
 import { Application } from "oak";
 import { serve } from "std";
 
-import router from "./app/routes/ws.ts";
-import createSocketListen from "./app/routes/socket.ts";
+import router from "./routes/ws.ts";
+import createSocketListen from "./routes/socket.ts";
 
 const io = new Server();
 const app = new Application();
