23f54caeb6847449e154b26a4839fe336ebd267e
Douglas Braga <30268535+dobraga@users.noreply.github.com>
2023-12-31 19:33:21 -0300

    feat: expire cache

--- app/static/utils/session.js
+++ app/static/utils/session.js
@@ -7,10 +7,10 @@ export default function session() {
 
   function getId() {
     const key = `card-game-${mapping.id}`;
-    const value = localStorage.getItem(key);
+    const value = getLocalStorageWithExpiration(key);
     if (value == undefined) {
       const id = crypto.randomUUID();
-      localStorage.setItem(key, id);
+      setLocalStorageWithExpiration(key, id, 60);
       return id;
     }
     return value;
@@ -18,13 +18,13 @@ export default function session() {
 
   function getName() {
     const key = `card-game-${mapping.name}`;
-    const value = localStorage.getItem(key);
+    const value = getLocalStorageWithExpiration(key);
     if (value == undefined) {
       let username = null
       while (username == null || username == undefined || username == '' || username == 0) {
         username = prompt("Please enter your name").trim();
       }
-      localStorage.setItem(key, username);
+      setLocalStorageWithExpiration(key, username, 60);
       return username;
     }
     return value;
@@ -41,7 +41,7 @@ export default function session() {
 
   function getCards() {
     const key = `card-game-${mapping.cards}`;
-    const value = localStorage.getItem(key);
+    const value = getLocalStorageWithExpiration(key);
     if (value == undefined) {
       return [];
     }
@@ -55,11 +55,38 @@ export default function session() {
     }
     const value = JSON.stringify({ cards: cards });
     console.log(`== store cards: ${value}`)
-    localStorage.setItem(key, value);
+    setLocalStorageWithExpiration(key, value, 60);
   }
 
   function getPoints() {}
   function storePoints() {}
 
   return { getId, getName, clear, getCards, storeCards, getPoints, storePoints };
+}
+
+
+function setLocalStorageWithExpiration(key, value, expirationMinutes) {
+  const now = new Date();
+  const expirationTime = now.getTime() + expirationMinutes * 60 * 1000; // Convert minutes to milliseconds
+  const data = {
+    value: value,
+    expirationTime: expirationTime,
+  };
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+// Function to get data from localStorage and check expiration
+function getLocalStorageWithExpiration(key) {
+  const storedData = localStorage.getItem(key);
+  if (storedData) {
+    const parsedData = JSON.parse(storedData);
+    const now = new Date();
+    if (now.getTime() < parsedData.expirationTime) {
+      return parsedData.value;
+    } else {
+      // Data has expired, remove it from localStorage
+      localStorage.removeItem(key);
+    }
+  }
+  return null;
 }
